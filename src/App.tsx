25f41a1ca8dead8c830ac464556852a652ef03e9
serjo <serjo-study@ya.ru>
2025-08-09 17:36:51 +0300

    feat: add new routs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import AuthorizationPage from "./pages/AuthorizationPage/AuthorizationPage"
 import AdminPage from "./pages/AdminPage/AdminPage"
+import SOPDPage from "./pages/SOPDPage/SOPDPage"
+import LetterTemplatePage from "./pages/LetterTemplatePage/LetterTemplatePage"
+import TestPage from "./pages/TestPage/TestPage"
 
 
 function App() {
@@ -9,10 +12,14 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
+        <Route path="/test" element={<TestPage/>} ></Route>
+        <Route path="*" element={<TestPage/>} />
+
+
         <Route path="/auth" element={<AuthorizationPage/>} ></Route>
         <Route path="/admin" element={<AdminPage/>}></Route>
-        {/* <Route path="/admin/sopd" element={<SOPDPage/>}></Route> */}
-        {/* <Route path="/admin/letter" element={<TemplateLetterPage/>}></Route> */}
+        <Route path="/admin/sopd" element={<SOPDPage/>}></Route>
+        <Route path="/admin/letter" element={<LetterTemplatePage/>}></Route>
 
       </Routes>
     </BrowserRouter>
