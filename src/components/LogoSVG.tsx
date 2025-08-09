7f4706368c2ac321162b0041d464e4e1480889ac
serjo <serjo-study@ya.ru>
2025-08-09 12:59:55 +0300

    feat: add logo component

---
+++ src/components/LogoSVG.tsx
@@ -0,0 +1,22 @@
+function LogoSVG() {
+  return (
+    <>
+      <div>
+          <svg width="83" height="36" viewBox="0 0 83 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_555_597)">
+              <path d="M72.7612 8.86615H53.9588V13.0406H61.2759V27.7503H65.4472V13.0438H72.7643V8.86615H72.7612Z" fill="black"></path>
+              <path d="M80.9811 8.80324H76.6871V27.7063H80.9811V8.80324Z" fill="black"></path>
+              <path d="M20.0881 15.1294H32.5517V21.5028H20.0881V34.0293H13.7493V21.5028H1.28572V15.1294H13.7493V2.60288H20.0881V15.1294ZM38.8748 2.57143V34.0293H45.2135V2.57143H38.8748Z" fill="#12A5DF"></path>
+            </g>
+            <defs>
+              <clipPath id="clip0_555_597">
+                <rect width="79.7143" height="31.4579" fill="white" transform="translate(1.28572 2.57143)"></rect>
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
+    </>
+  )
+}
+
+export default LogoSVG
