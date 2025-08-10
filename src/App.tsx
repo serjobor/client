63c881a3607bab3802be66aa4f3556a20bd2d474
serjo <serjo-study@ya.ru>
2025-08-10 14:35:35 +0300

    feat: add index.ts files to short paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import AuthorizationPage from "./pages/AuthorizationPage/AuthorizationPage"
-import AdminPage from "./pages/AdminPage/AdminPage"
-import SOPDPage from "./pages/SOPDPage/SOPDPage"
-import LetterTemplatePage from "./pages/LetterTemplatePage/LetterTemplatePage"
-import TestPage from "./pages/TestPage/TestPage"
-import ErrorPage from "./pages/ErrorPage/ErrorPage"
+import AuthorizationPage from "./pages/AuthorizationPage"
+import AdminPage from "./pages/AdminPage"
+import SOPDPage from "./pages/SOPDPage"
+import LetterTemplatePage from "./pages/LetterTemplatePage"
+import TestPage from "./pages/TestPage"
+import ErrorPage from "./pages/ErrorPage"
 
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
         <Route path="*" element={<TestPage/>} />
 
         <Route path="/auth" element={<AuthorizationPage/>} ></Route>
+        
         <Route path="/admin" element={<AdminPage/>}></Route>
         <Route path="/admin/sopd" element={<SOPDPage/>}></Route>
         <Route path="/admin/letter" element={<LetterTemplatePage/>}></Route>
